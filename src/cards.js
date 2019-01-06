ebeef12ea3474322b1b35ac9b30169cb92c8d645
Chris Otto <chris.otto6@gmail.com>
2019-01-06 11:22:17 -0600

    Running the files through Prettier

--- src/cards.js
+++ src/cards.js
@@ -1,50 +1,65 @@
-'use strict';
+"use strict";
 
 export class CardDeck {
-    constructor() {
-        this.restore();
+  constructor() {
+    this.restore();
+  }
+
+  /*
+   * Restore re-creates the cardDeck object to an empty array and rebuilds the deck
+   * based on the standard 52 card deck.
+   */
+  restore() {
+    this.cardDeck = [];
+    const cardValue = [
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      "Jack",
+      "Queen",
+      "King",
+      "Ace"
+    ];
+    const cardSuit = ["Spades", "Hearts", "Diamonds", "Clubs"];
+
+    /* Loop through the card suit and card value to push those two items together in the cardDeck array */
+    for (let suit in cardSuit) {
+      for (let value in cardValue) {
+        this.cardDeck.push(`${cardValue[value]} of ${cardSuit[suit]}`);
+      }
+    }
+  }
+
+  /*
+   * Shuffle uses the cardDeck object and shuffles it. Utilizing the Fisher-Yates algorithm for shuffling
+   * the array of cards.
+   */
+  shuffle() {
+    const { cardDeck } = this;
+    let cdl = cardDeck.length;
+    let i;
+
+    // Loop through the deck, using Math.random() swap the two items locations in the array
+    while (cdl) {
+      i = Math.floor(Math.random() * cdl--);
+
+      [cardDeck[cdl], cardDeck[i]] = [cardDeck[i], cardDeck[cdl]];
     }
 
-    /*
-     * Restore re-creates the cardDeck object to an empty array and rebuilds the deck
-     * based on the standard 52 card deck.
-     */
-    restore() {
-        this.cardDeck = [];
-        const cardValue = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"];
-        const cardSuit = ["Spades", "Hearts", "Diamonds","Clubs"];
+    return this;
+  }
 
-        /* Loop through the card suit and card value to push those two items together in the cardDeck array */
-        for (let suit in cardSuit) {
-            for (let value in cardValue) {
-                this.cardDeck.push(`${cardValue[value]} of ${cardSuit[suit]}`)
-            }
-        }
-    }
+  /*
+   * Utilizing array shift, we can retrieve and remove the first element of the cardDeck array
+   */
 
-    /* 
-     * Shuffle uses the cardDeck object and shuffles it. Utilizing the Fisher-Yates algorithm for shuffling
-     * the array of cards.
-    */
-   shuffle() {
-       const { cardDeck } = this;
-       let cdl = cardDeck.length;
-       let i;
-
-       // Loop through the deck, using Math.random() swap the two items locations in the array
-       while(cdl) {
-            i = Math.floor(Math.random() * cdl--);
-
-            [cardDeck[cdl], cardDeck[i]] = [cardDeck[i], cardDeck[cdl]];
-       }
-
-       return this;
-   }
-
-   /*
-    * Utilizing array shift, we can retrieve and remove the first element of the cardDeck array
-    */ 
-    deal() {
-        return this.cardDeck.shift();
-   }
+  deal() {
+    return this.cardDeck.shift();
+  }
 }
--- src/main.js
+++ src/main.js
@@ -1,100 +1,100 @@
-import { CardDeck } from './cards';
+import { CardDeck } from "./cards";
 
 let playerArea = document.getElementById("playerArea");
 let deck = new CardDeck();
 let players;
 
 document.getElementById("addPlayers").addEventListener("click", e => {
-    // Get the value the user entered for the number of players and clear the player list area if there was a previous session
-    players = document.getElementById("players").value;
-    players = parseFloat(players);
-    clearArea();
+  // Get the value the user entered for the number of players and clear the player list area if there was a previous session
+  players = document.getElementById("players").value;
+  players = parseFloat(players);
+  clearArea();
 
-    // Make sure the number added is a positive number greater than zero.
-    if (players <= 0) {
-        alert('The numbers of players has to be greater than 0.');
-        document.getElementById("players").value = "";
-    }
-    else if (players > 52) {
-        alert('The numbers of players has to be less than or equal to 52.');
-         document.getElementById("players").value = "";
-    }
-    else {
-        // Shuffle and deal the cards out to the amount of players
-        deck.shuffle();
-        let dealt = dealToPlayers(deck, players);
+  // Make sure the number added is a positive number greater than zero.
+  if (players <= 0) {
+    alert("The numbers of players has to be greater than 0.");
+    document.getElementById("players").value = "";
+  } else if (players > 52) {
+    alert("The numbers of players has to be less than or equal to 52.");
+    document.getElementById("players").value = "";
+  } else {
+    // Shuffle and deal the cards out to the amount of players
+    deck.shuffle();
+    let dealt = dealToPlayers(deck, players);
 
-        // Take the players and cards and apply their values to the page
-        playerToClient(dealt);
-    }
-        
-    // There is no other user interaction with the deck so restore it
-    deck.restore();
+    // Take the players and cards and apply their values to the page
+    playerToClient(dealt);
+  }
+
+  // There is no other user interaction with the deck so restore it
+  deck.restore();
 });
 
 // If the user presses Enter instead of clicking the button, execute a click on the button
 document.getElementById("players").addEventListener("keyup", e => {
-    e.preventDefault();
-    if(e.keyCode === 13) {
-        document.getElementById("addPlayers").click();
-    }
+  e.preventDefault();
+  if (e.keyCode === 13) {
+    document.getElementById("addPlayers").click();
+  }
 });
 
 /*
  * Clears the area where the hands of each player are displayed
  */
 function clearArea() {
-    while (playerArea.firstChild) {
-        playerArea.removeChild(playerArea.firstChild);
-    }
+  while (playerArea.firstChild) {
+    playerArea.removeChild(playerArea.firstChild);
+  }
 }
 
-/* 
+/*
  * Takes in a shuffled deck, utilizing the cardDeck classes deal function to create a two dimensional array to house the players and their cards
  */
 function dealToPlayers(deckOfCards, numberOfPlayers) {
-    let cdl = deckOfCards.cardDeck.length;
-    let dealtDeck = [];
+  let cdl = deckOfCards.cardDeck.length;
+  let dealtDeck = [];
 
-    // Create arrays within the dealtDeck array that will be used to house the cards as they're dealt
-    for(let i = 0; i < numberOfPlayers; i++) {
-        dealtDeck[i] = [];
+  // Create arrays within the dealtDeck array that will be used to house the cards as they're dealt
+  for (let i = 0; i < numberOfPlayers; i++) {
+    dealtDeck[i] = [];
+  }
+
+  // Use the deck length to drive the dealing, loop through the dealtDeck array for the player and deal cards into each one of their arrays. If the rest
+  // of the cards run out before we finish, put an empty string in the remainder of the last positions of the array for the rest of the players.
+  while (cdl) {
+    for (let j = 0; j < dealtDeck.length; j++) {
+      if (cdl > 0) {
+        dealtDeck[j].push(deckOfCards.deal());
+        cdl--;
+      } else {
+        dealtDeck[j].push(" ");
+      }
     }
-    
-    // Use the deck length to drive the dealing, loop through the dealtDeck array for the player and deal cards into each one of their arrays. If the rest
-    // of the cards run out before we finish, put an empty string in the remainder of the last positions of the array for the rest of the players.
-    while (cdl) {
-        for(let j = 0; j < dealtDeck.length; j++) {
-            if (cdl > 0) {
-                dealtDeck[j].push(deckOfCards.deal()); 
-                cdl--;
-            }
-            else {
-                dealtDeck[j].push(" ");
-            }
-        }
-    }
-    return dealtDeck;
+  }
+  return dealtDeck;
 }
 
 /*
  * Taking the dealtDeck from dealToPlayers, display each player and their cards to the user
  */
 function playerToClient(deckOfCards) {
-    let dealer = deckOfCards;
+  let dealer = deckOfCards;
 
-    // Loop through the two dimensional array and create 'cards' for players with their hands in a list below the card header
-    for(let k = 0; k < dealer.length; k++){
-        let playerDiv = document.createElement('div');
-        playerDiv.setAttribute("class", "card");
+  // Loop through the two dimensional array and create 'cards' for players with their hands in a list below the card header
+  for (let k = 0; k < dealer.length; k++) {
+    let playerDiv = document.createElement("div");
+    playerDiv.setAttribute("class", "card");
 
-        let temp = "<div class = 'card-header'>\nPlayer " + (k + 1) + "\n</div>\n<ul class = 'list-group list-group-flush'>";
+    let temp =
+      "<div class = 'card-header'>\nPlayer " +
+      (k + 1) +
+      "\n</div>\n<ul class = 'list-group list-group-flush'>";
 
-        for(let l = 0; l < dealer[k].length; l++) {
-            temp += "\n<li class = 'list-group-item'>" + dealer[k][l] + "</li>";
-        }
-
-        playerArea.appendChild(playerDiv);
-        playerDiv.innerHTML = temp;        
+    for (let l = 0; l < dealer[k].length; l++) {
+      temp += "\n<li class = 'list-group-item'>" + dealer[k][l] + "</li>";
     }
+
+    playerArea.appendChild(playerDiv);
+    playerDiv.innerHTML = temp;
+  }
 }
